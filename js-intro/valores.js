8e801bcd2e3fd22effbf0b7546681c9332132038
Sofia Alfaro <alfaros.sofia@gmail.com>
2023-07-04 23:55:19 -0500

    update: intro

---
+++ js-intro/valores.js
@@ -0,0 +1,12 @@
+console.log("typeof: devuelve el tipo de variable")
+// Tipos de datos primitivos
+typeof 5  // 'number'
+typeof "hola" // 'string'
+typeof true  // 'boolean'
+typeof null  // 'object'
+typeof undefined // 'undefined'
+
+// Tipos de datos de objeto 
+typeof console.log  // 'function'
+typeof {tipo: "objeto"} // 'object'
+typeof [1,2,3,4]  // 'object'
---
+++ js-intro/variables.js
@@ -0,0 +1,9 @@
+var nombre;
+nombre = "sofia";
+
+var electronicos =["Celular", "Tablet", "Computadora"];
+
+var persona={
+    nombre:"Sofia",
+    edad:21
+}
